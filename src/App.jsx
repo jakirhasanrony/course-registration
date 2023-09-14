7713d240211a3e6db459fb81eafbd06faaa5a6fd
Md. Jakir Hasan Rony <jakir15-14584@diu.edu.bd>
2023-09-14 19:13:04 +0600

    Fake Data added and Tailwind Setup added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// eslint-disable-next-line no-unused-vars
+import reactLogo from './assets/react.svg'
+// eslint-disable-next-line no-unused-vars
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
